9252261143cb1abea6eba9d91eeb9915a0376de3
Timothy Wyld <timwyld@gmail.com>
2019-10-22 17:02:12 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var obj = {key: 'value'};
+
+function updateObjectWithKeyAndValue(object,key,value){
+ return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+  obj[key] = 'value';
+  return obj;
+}
